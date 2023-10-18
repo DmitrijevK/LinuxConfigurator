ef84ad2d999695b5477932ce518ae5de3fd8d18e
DmitrijevK <70750217+DmitrijevK@users.noreply.github.com>
2023-10-18 23:50:46 +0300

    Update Kali_secconf.sh

--- Kali_secconf.sh
+++ Kali_secconf.sh
@@ -29,3 +29,30 @@ echo "Есть ли у вас конфиг? Y\N"
 read oovpnconfigname
 sudo openvpn --config $oovpnconfigname
 }
+
+OperSystem()
+{
+#Antimac
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install macchanger -y
+echo "Укажите wlan из списка в ipconfig"
+read wlanname
+sudo ifconfig wlan0 down && sudo macchanger -r $wlanname && sudo ifconfig $wlanname up
+echo "МАК сменен"
+#AntiDns
+echo "Введите ваш DNS SERVER ПО ПРИНЦИПУ: nameserver 8.8.8.8"
+read NEW_DNS_SERVER
+
+dCONFIG_FILE="/etc/resolv.conf"
+if [ ! -f $dCONFIG_FILE ]; then
+    echo "Файл $dCONFIG_FILE не найден. Пожалуйста, убедитесь, что он существует."
+    exit 1
+fi
+if grep -q "^$NEW_DNS_SERVER" $CONFIG_FILE; then
+    echo "Строка '$NEW_DNS_SERVER' уже существует в файле."
+    exit 0
+fi
+echo $NEW_DNS_SERVER | sudo tee -a $CONFIG_FILE > /dev/null
+echo "Строка '$NEW_DNS_SERVER' успешно добавлена в файл $dCONFIG_FILE."
+
+
+}
